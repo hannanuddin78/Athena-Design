98237fad34102e321584e28e7a8173e158b1a2fd
nihaltowfiq <nihaltowfiq@gmail.com>
2020-11-19 13:59:14 +0600

    bug fixed

--- src/Components/Home/ContactUs/ContactUs.js
+++ src/Components/Home/ContactUs/ContactUs.js
@@ -20,7 +20,7 @@ const ContactUs = () => {
                                 It is a long established fact that a reader will be distracted by the
                                 readable content of a page when looking at its layout. The point of
                                 using Lorem Ipsum is that it has a more-or-less normal distribution
-                                of letter.
+                                of letter. 
                             </p>
                             <button className="contactBtn mt-3">Contact Us</button>
                         </div>
