3aedd3dfeb6689d194aa76d3fde09d800416032d
nihaltowfiq <nihaltowfiq@gmail.com>
2020-11-19 10:55:28 +0600

    first commit

--- src/Components/Home/Achievements/Achievements.js
+++ src/Components/Home/Achievements/Achievements.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import { Col, Container, Row } from 'react-bootstrap';
 import './Achievements.css';
 
 const Achievements = () => {
     return (
         <div className="section">
-            <h1>Our Achievements component</h1>
+            <Container>
+                <Row>
+                    <Col md={5}>
+                        <h2 className="font-weight-bold">Our Achievements</h2>
+                        <p className="mt-3 text-secondary">
+                            It is a long established fact that a reader will be distracted by
+                            the readable content of a page when looking at its layout. The
+                            point of using Lorem Ipsum is that it has a more-or-less normal
+                            distribution of letter
+                        </p>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
