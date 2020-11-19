3493ed62bb2aa5b366691bd23cb981753d673515
Rasel5267 <mahadiul09@gmail.com>
2020-11-19 18:49:57 +0600

    responsive footer design

--- src/Components/Home/Footer/Footer.js
+++ src/Components/Home/Footer/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
         <div className="section">
            <Container className="my-3">
                <Row>
-                   <Col md={5} className="px-5">
+                   <Col md={5} className="px-5 footer-icon mb-3">
                        <img style={{width:"120px"}} src={logo} alt=""/>
                        <p className="iconStyle">
                            <a href="#"><img src={facebook} alt=""/></a>
@@ -22,26 +22,26 @@ const Footer = () => {
                        </p>
                    </Col>
                    <Col md={7}>
-                        <Row className="text-secondary">
-                            <Col sm={12} md={4}>
+                        <Row className="text-secondary footer-des">
+                            <Col sm={12} md={4} className="mb-3">
                                 <span>Feature</span> <br/>
                                 <span>Enterprise</span> <br/>
                                 <span>Pricing</span>
                             </Col>
-                            <Col sm={12} md={4}>
+                            <Col sm={12} md={4} className="mb-3">
                                 <span>About Us</span> <br/>
                                 <span>Terms of Service</span> <br/>
                                 <span>Security</span> <br/>
                                 <span>Login</span>
                             </Col>
-                            <Col sm={12} md={4}>
+                            <Col sm={12} md={4} className="mb-3">
                                 <span>Blog</span> <br/>
                                 <span>Help Center</span> <br/>
                                 <span>Contact Us</span> <br/>
                                 <span>Status</span>                            
                             </Col>
                         </Row>
-                        <Row className="text-secondary mt-3">
+                        <Row className="text-secondary mt-3 text-center">
                             <Col><span>Copyright @ 2020 All rights reserved, SCIC-TEAM9</span></Col>
                         </Row>
                    </Col>
